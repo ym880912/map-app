1cfbd7be3612864d78b5f376b1d00a01f5e9bfd6
YoheiMiyagawa <gcyuri0912@gmail.com>
2021-05-05 15:32:01 +0900

    スマホサイズテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,17 +18,18 @@ export default function Home ({ allDesk }) {
     height: undefined
   })
 
+  function handleResize () {
+    // Set window width/height to state
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+
   useEffect(() => {
     // only execute all the code below in client side
     if (typeof window !== 'undefined') {
       // Handler to call on window resize
-      function handleResize () {
-        // Set window width/height to state
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight
-        })
-      }
 
       // Add event listener
       window.addEventListener('resize', handleResize)
