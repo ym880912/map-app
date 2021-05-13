e3960a7f202c1eef536a5ee81870e02c2d67789d
YoheiMiyagawa <gcyuri0912@mail.com>
2021-05-13 09:08:19 +0900

    ファイル調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { SearchBar } from '../components/SearchBar'
+import { HeaderBar, HEADERBAR_HEIGHT } from '../components/HeaderBar'
+import { makeStyles, fade, Theme } from '@material-ui/core/styles'
+import { InformationModal } from '../components/InformationModal'
 import { Map } from '../components/Map'
-import { Layout } from '../components/Layout'
 
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {},
+  content: {}
+}))
 
 export default function Home () {
+  const classes = useStyles()
 
   return (
-    <Layout>
-      <Map />
-    </Layout>
+    <div className={classes.root}>
+      <HeaderBar />
+      <main className={classes.content}>
+        <Map />
+      </main>
+      <SearchBar />
+      <InformationModal />
+    </div>
   )
 }
